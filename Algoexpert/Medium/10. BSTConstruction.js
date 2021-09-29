145778b31d18ccd0c89c80b1ccdacab7a6460bca
Unbuttun <unbuttun@gmail.com>
2021-09-29 23:25:40 +0530

    update

--- Algoexpert/Medium/10. BSTConstruction.js
+++ Algoexpert/Medium/10. BSTConstruction.js
@@ -14,12 +14,10 @@ class BST {
       } else {
         this.left.insert(value);
       }
+    } else if (!this.right) {
+      this.right = new BST(value);
     } else {
-      if (!this.right) {
-        this.right = new BST(value);
-      } else {
-        this.right.insert(value);
-      }
+      this.right.insert(value);
     }
     return this;
   }
@@ -30,18 +28,16 @@ class BST {
     if (value < this.value) {
       if (this.left === null) {
         return false;
-      } else {
-        return this.left.contains(value);
       }
-    } else if (value > this.value) {
+      return this.left.contains(value);
+    }
+    if (value > this.value) {
       if (this.right === null) {
         return false;
-      } else {
-        return this.right.contains(value);
       }
-    } else {
-      return true;
+      return this.right.contains(value);
     }
+    return true;
   }
 
   remove(value, parent = null) {
@@ -53,26 +49,24 @@ class BST {
       if (this.right) {
         this.right.remove(value, this);
       }
-    } else {
-      if (this.left && this.right) {
-        this.value = this.right.getMinValue();
-        this.right.remove(this.value, this);
-      } else if (!parent) {
-        if (this.left) {
-          this.value = this.left.value;
-          this.right = this.left.right;
-          this.left = this.left.left;
-        } else if (this.right) {
-          this.value = this.right.value;
-          this.right = this.right.right;
-          this.left = this.right.left;
-        } else {
-        }
-      } else if (parent.left === this) {
-        parent.left = this.left !== null ? this.left : this.right;
-      } else if (parent.right === this) {
-        parent.right = this.left !== null ? this.left : this.right;
+    } else if (this.left && this.right) {
+      this.value = this.right.getMinValue();
+      this.right.remove(this.value, this);
+    } else if (!parent) {
+      if (this.left) {
+        this.value = this.left.value;
+        this.right = this.left.right;
+        this.left = this.left.left;
+      } else if (this.right) {
+        this.value = this.right.value;
+        this.right = this.right.right;
+        this.left = this.right.left;
+      } else {
       }
+    } else if (parent.left === this) {
+      parent.left = this.left !== null ? this.left : this.right;
+    } else if (parent.right === this) {
+      parent.right = this.left !== null ? this.left : this.right;
     }
     return this;
   }
@@ -80,8 +74,7 @@ class BST {
   getMinValue() {
     if (this.left === null) {
       return this.value;
-    } else {
-      return this.left.getMinValue();
     }
+    return this.left.getMinValue();
   }
 }
