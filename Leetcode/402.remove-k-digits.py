d4229eb75e3bb7e96c2133759237ad2f501321a0
Spark <79936503+spa5k@users.noreply.github.com>
2022-02-22 22:32:41 +0530

    commit for today

---
+++ Leetcode/402.remove-k-digits.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=402 lang=python3
+#
+# [402] Remove K Digits
+#
+
+# @lc code=start
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        
+# @lc code=end
+
