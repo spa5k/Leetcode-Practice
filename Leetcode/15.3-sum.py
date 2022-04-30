48fbc2dc0086eff8cfd59319b2379d95ee00614b
Spark <79936503+spa5k@users.noreply.github.com>
2022-05-01 00:03:35 +0530

    some commit

--- Leetcode/15.3-sum.py
+++ Leetcode/15.3-sum.py
@@ -7,24 +7,23 @@
 # @lc code=start
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res=[]
+        res = []
         nums.sort()
 
-        for i,a in enumerate(nums):
-            if i >0 and a ==nums[i-1]:
-              continue
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i-1]:
+                continue
 
-            l,r=i+1,len(nums)-1
+            l, r = i+1, len(nums)-1
 
-            while l<r:
-              three_sum=a+nums[l]+nums[r]
+            while l < r:
+                three_sum = a+nums[l]+nums[r]
 
-              if three_sum==0:
-                  res.append([a,nums[l],nums[r]])
-              elif three_sum >0:
-                r-=1
-              else:
-                l+=1
+                if three_sum == 0:
+                    res.append([a, nums[l], nums[r]])
+                elif three_sum > 0:
+                    r -= 1
+                else:
+                    l += 1
         return res
 # @lc code=end
-
