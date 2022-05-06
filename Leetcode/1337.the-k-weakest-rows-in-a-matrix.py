ee44cb5a245d7ffe67cebb4c0fe0520fd9709269
Spark <79936503+spa5k@users.noreply.github.com>
2022-05-06 23:11:01 +0530

    more questions

--- Leetcode/1337.the-k-weakest-rows-in-a-matrix.py
+++ Leetcode/1337.the-k-weakest-rows-in-a-matrix.py
@@ -84,32 +84,35 @@
 #
 
 # @lc code=start
+import heapq
+from typing import List
+
+
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        ans = []
+        heap = []
 
         for i, row in enumerate(mat):
-            r = self.bs(row)
-            ans.append((i, r))
-        ans.sort(key=lambda x: (x[1], x[0]))
-        return [x[0] for x in ans[:k]]
+            r = self.bs(self, row)
+            heapq.heappush(heap, (-r, -i, i))
+        while len(heap) > k:
+            heapq.heappop(heap)
+        res = [0]*k
+        for i in range(k-1, -1, -1):
+            res[i] = heapq.heappop(heap)[2]
+        return res
 
     def bs(self, row):
-        start = 0
-        end = len(row)-1
-        if start > end:
-            return
-
-        l, r = start, end
-
-        while l <= r:
-            mid = (l+r)//2
-
+        left, right = 0, len(row)
+        while left < right:
+            mid = left + (right-left)//2
             if row[mid] == 1:
-                l = mid+1
-            elif row[mid] == 0:
-                r = mid-1
-        if r < 0 or row[r] != 1:
-            return -1
-        return r
+                left = mid + 1
+            else:
+                right = mid
+        return left
+
+
+print(Solution.kWeakestRows(Solution, [[1, 1, 0, 0, 0], [
+      1, 1, 1, 1, 0], [1, 0, 0, 0, 0], [1, 1, 0, 0, 0], [1, 1, 1, 1, 1]], 3))
 # @lc code=end
