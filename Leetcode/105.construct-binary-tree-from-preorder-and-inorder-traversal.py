7e8f6734d4e1e3ce520b28ca98c086845d90ba55
Spark <79936503+spa5k@users.noreply.github.com>
2022-07-17 15:01:54 +0530

    more questions

---
+++ Leetcode/105.construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,94 @@
+# @before-stub-for-debug-begin
+from typing import *
+
+from python3problem105 import *
+
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode id=105 lang=python3
+#
+# [105] Construct Binary Tree from Preorder and Inorder Traversal
+#
+# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+#
+# algorithms
+# Medium (58.24%)
+# Likes:    10093
+# Dislikes: 271
+# Total Accepted:    796.4K
+# Total Submissions: 1.3M
+# Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+#
+# Given two integer arrays preorder and inorder where preorder is the preorder
+# traversal of a binary tree and inorder is the inorder traversal of the same
+# tree, construct and return the binary tree.
+#
+#
+# Example 1:
+#
+#
+# Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+# Output: [3,9,20,null,null,15,7]
+#
+#
+# Example 2:
+#
+#
+# Input: preorder = [-1], inorder = [-1]
+# Output: [-1]
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= preorder.length <= 3000
+# inorder.length == preorder.length
+# -3000 <= preorder[i], inorder[i] <= 3000
+# preorder and inorder consist of unique values.
+# Each value of inorder also appears in preorder.
+# preorder is guaranteed to be the preorder traversal of the tree.
+# inorder is guaranteed to be the inorder traversal of the tree.
+#
+#
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+        # root = preorder[0]
+        # left = []
+
+        # for i in inorder:
+        #     if i == root:
+        #         break
+        #     left.append(i)
+
+        # print(left)
+
+        # for i in range(len(left)+1):
+        #     preorder.pop(0)
+        # right = preorder
+        # root = TreeNode(root)
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        return root
+
+
+# @lc code=end
