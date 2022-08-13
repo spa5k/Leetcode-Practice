a762dacacb9fc810564e162566a9aed86c3a811b
Spark <79936503+spa5k@users.noreply.github.com>
2022-08-13 23:34:54 +0530

    moving leetcode python to a new folder

---
+++ LeetcodePython/337.house-robber-iii.py
@@ -0,0 +1,72 @@
+#
+# @lc app=leetcode id=337 lang=python3
+#
+# [337] House Robber III
+#
+# https://leetcode.com/problems/house-robber-iii/description/
+#
+# algorithms
+# Medium (53.62%)
+# Likes:    6708
+# Dislikes: 97
+# Total Accepted:    303K
+# Total Submissions: 564.3K
+# Testcase Example:  '[3,2,3,null,3,null,1]'
+#
+# The thief has found himself a new place for his thievery again. There is only
+# one entrance to this area, called root.
+#
+# Besides the root, each house has one and only one parent house. After a tour,
+# the smart thief realized that all houses in this place form a binary tree. It
+# will automatically contact the police if two directly-linked houses were
+# broken into on the same night.
+#
+# Given the root of the binary tree, return the maximum amount of money the
+# thief can rob without alerting the police.
+#
+#
+# Example 1:
+#
+#
+# Input: root = [3,2,3,null,3,null,1]
+# Output: 7
+# Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+#
+#
+# Example 2:
+#
+#
+# Input: root = [3,4,5,1,3,null,1]
+# Output: 9
+# Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+#
+#
+#
+# Constraints:
+#
+#
+# The number of nodes in the tree is in the range [1, 10^4].
+# 0 <= Node.val <= 10^4
+#
+#
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root):
+        def with_without_rob(root):
+
+            if root:
+                with_l, without_l = with_without_rob(root.left)
+                with_r, without_r = with_without_rob(root.right)
+                return (root.val + without_l + without_r, max(with_l, without_l) + max(with_r, without_r))
+            return (0, 0)
+
+        return max(with_without_rob(root))
+# @lc code=end
