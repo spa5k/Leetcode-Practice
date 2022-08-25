9b4f03fc66118deaec9bd4026a897bd2e78fb6fb
Spark <79936503+spa5k@users.noreply.github.com>
2022-08-25 20:15:34 +0530

    more questions

---
+++ Leetcode/226.invert-binary-tree.cpp
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode id=226 lang=cpp
+ *
+ * [226] Invert Binary Tree
+ *
+ * https://leetcode.com/problems/invert-binary-tree/description/
+ *
+ * algorithms
+ * Easy (72.67%)
+ * Likes:    9502
+ * Dislikes: 132
+ * Total Accepted:    1.2M
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[4,2,7,1,3,6,9]'
+ *
+ * Given the root of a binary tree, invert the tree, and return its root.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [4,2,7,1,3,6,9]
+ * Output: [4,7,2,9,6,3,1]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [2,1,3]
+ * Output: [2,3,1]
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// struct TreeNode
+// {
+//   int val;
+//   TreeNode *left;
+//   TreeNode *right;
+//   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+class Solution
+{
+public:
+  TreeNode *invertTree(TreeNode *root)
+  {
+    if (!root)
+    {
+      return nullptr;
+    }
+    TreeNode *tmp = root->left;
+    root->left = root->right;
+    root->right = tmp;
+
+    invertTree(root->left);
+    invertTree(root->right);
+    return root;
+  }
+};
+// @lc code=end
---
+++ Leetcode/98.validate-binary-search-tree.cpp
@@ -0,0 +1,103 @@
+/*
+ * @lc app=leetcode id=98 lang=cpp
+ *
+ * [98] Validate Binary Search Tree
+ *
+ * https://leetcode.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (31.45%)
+ * Likes:    12125
+ * Dislikes: 990
+ * Total Accepted:    1.6M
+ * Total Submissions: 5.2M
+ * Testcase Example:  '[2,1,3]'
+ *
+ * Given the root of a binary tree, determine if it is a valid binary search
+ * tree (BST).
+ *
+ * A valid BST is defined as follows:
+ *
+ *
+ * The left subtree of a node contains only nodes with keys less than the
+ * node's key.
+ * The right subtree of a node contains only nodes with keys greater than the
+ * node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [2,1,3]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node's value is 5 but its right child's value is
+ * 4.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [1, 10^4].
+ * -2^31 <= Node.val <= 2^31 - 1
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// struct TreeNode
+// {
+//   int val;
+//   TreeNode *left;
+//   TreeNode *right;
+//   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+class Solution
+{
+public:
+  bool isValidBST(TreeNode *root)
+  {
+    return isValid(root, nullptr, nullptr);
+  }
+  bool isValid(TreeNode *root, int *left, int *right)
+  {
+    if (root == nullptr)
+    {
+      return true;
+    }
+    if (right && root->val >= *right)
+    {
+      return false;
+    }
+    if (left && root->val <= *left)
+    {
+      return false;
+    }
+    bool l = isValid(root->left, left, &(root->val));
+    bool r = isValid(root->right, &(root->val), right);
+    return l && r;
+  }
+};
+// @lc code=end
