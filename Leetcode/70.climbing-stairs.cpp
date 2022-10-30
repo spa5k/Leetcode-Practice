191e0cc210ff4a5f2251e377b7799adfad836a3e
Spark <79936503+spa5k@users.noreply.github.com>
2022-10-30 15:51:19 +0530

    more questions

---
+++ Leetcode/70.climbing-stairs.cpp
@@ -0,0 +1,68 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem70.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode id=70 lang=cpp
+ *
+ * [70] Climbing Stairs
+ *
+ * https://leetcode.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (51.72%)
+ * Likes:    15088
+ * Dislikes: 443
+ * Total Accepted:    2M
+ * Total Submissions: 3.8M
+ * Testcase Example:  '2'
+ *
+ * You are climbing a staircase. It takes n steps to reach the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can
+ * you climb to the top?
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: n = 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: n = 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= n <= 45
+ *
+ *
+ */
+// #include <headers.h>
+// @lc code=start
+class Solution
+{
+public:
+  int climbStairs(int n)
+  {
+  }
+};
+// @lc code=end
--- cpp/1.kthLargestNumber.cpp
+++ cpp/1.kthLargestNumber.cpp
@@ -1,24 +1,40 @@
-#include <headers.h>
+#include <iostream>
+#include <vector>
+#include <string>
 
-int findKthLargestNumber(vector<int> &nums, int k)
+using namespace std;
+
+int main()
 {
-  priority_queue<int, vector<int>, greater<int>> pq;
-  int count = 0;
-  for (int i = 0; i < nums.size(); i++)
+  vector<string> msg{"Hello", "World", "from", "VS Code", "and the C++ extension!"};
+
+  for (const string &word : msg)
   {
-    if (count < k)
-    {
-      pq.push(nums[i]);
-      count++;
-    }
-    else
-    {
-      if (nums[i] > pq.top())
-      {
-        pq.pop();
-        pq.push(nums[i]);
-      }
-    }
+    cout << word << " ";
   }
-  return pq.top();
+  cout << endl;
 }
+// #include <headers.h>
+
+// int findKthLargestNumber(vector<int> &nums, int k)
+// {
+//   priority_queue<int, vector<int>, greater<int>> pq;
+//   int count = 0;
+//   for (int i = 0; i < nums.size(); i++)
+//   {
+//     if (count < k)
+//     {
+//       pq.push(nums[i]);
+//       count++;
+//     }
+//     else
+//     {
+//       if (nums[i] > pq.top())
+//       {
+//         pq.pop();
+//         pq.push(nums[i]);
+//       }
+//     }
+//   }
+//   return pq.top();
+// }
