5541c08fdf2f73b1958ab492db0e3a44dab46bc2
Spark <79936503+spa5k@users.noreply.github.com>
2023-03-04 19:27:50 +0530

    more questions

---
+++ LeetcodeJs/11.container-with-most-water.js
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=11 lang=javascript
+ *
+ * [11] Container With Most Water
+ *
+ * https://leetcode.com/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (54.07%)
+ * Likes:    22978
+ * Dislikes: 1222
+ * Total Accepted:    2M
+ * Total Submissions: 3.7M
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * You are given an integer array height of length n. There are n vertical
+ * lines drawn such that the two endpoints of the i^th line are (i, 0) and (i,
+ * height[i]).
+ *
+ * Find two lines that together with the x-axis form a container, such that the
+ * container contains the most water.
+ *
+ * Return the maximum amount of water a container can store.
+ *
+ * Notice that you may not slant the container.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: height = [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * Explanation: The above vertical lines are represented by array
+ * [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the
+ * container can contain is 49.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: height = [1,1]
+ * Output: 1
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * n == height.length
+ * 2 <= n <= 10^5
+ * 0 <= height[i] <= 10^4
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxVol = Math.min(height[left], height[right]) * (right - left);
+  while (left < right) {
+    let curr = Math.min(height[left], height[right]) * (right - left);
+    maxVol = Math.max(curr, maxVol);
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return maxVol;
+};
+
+
